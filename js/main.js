7ba945063987413f2553a4ec375133fa591ee7c6
Konrad Studzinski <kostudzinski@gmail.com>
2017-08-09 16:42:23 +0200

    NeonDone

--- js/main.js
+++ js/main.js
@@ -38,15 +38,43 @@ function scrollSlow() {
 
 function triggerNeon() {
     $('.omnie').click(function () {
-        $('.omniehead').addClass('neon-light');
+        function triggerTime1() {
+        $('.omniehead').addClass('neon-light')
+        };
+        setTimeout(triggerTime1, 1000)
+        function triggerRemove1() {
+        $('.omniehead').removeClass('neon-light')
+        };
+        setTimeout(triggerRemove1, 4000)
     });
     $('.umiejetnosci').click(function () {
+        function triggerTime2() {
         $('.skills').addClass('neon-light');
+        };
+        setTimeout(triggerTime2, 1000)   
+        function removeTrigger2() {
+        $('.skills').removeClass('neon-light');
+        };
+        setTimeout(removeTrigger2, 4000)
     });
     $('.portfolio').click(function () {
+        function triggerTime3() {
         $('.my-portfolio').addClass('neon-light');
+        }
+        setTimeout(triggerTime3, 1000)
+        function triggerRemove3() {
+        $('.my-portfolio').removeClass('neon-light');
+        }
+        setTimeout(triggerRemove3, 4000)
     });
     $('.kontakt').click(function () {
+        function triggerTime4() {
         $('.my-contact').addClass('neon-light');
+        }
+        setTimeout(triggerTime4, 1000)
+        function triggerRemove4() {
+        $('.my-contact').removeClass('neon-light');
+        }
+        setTimeout(triggerRemove4, 4000)
     });
 }
